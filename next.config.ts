9ab0c639b94eb1e7203c50d3d1cab1cadb41a65b
dsmmarket307 <juanfelipemontanogarcia@gmail.com>
2026-05-15 15:36:15 -0500

    revert: volver al estado anterior

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-﻿import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  reactCompiler: true,
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
