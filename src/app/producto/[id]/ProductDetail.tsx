82860adba6b4c131315e0d59aa6281f7307603bf
dsmmarket307 <juanfelipemontanogarcia@gmail.com>
2026-05-18 18:36:59 -0500

    fix: logos oficiales Nequi Daviplata

--- src/app/producto/[id]/ProductDetail.tsx
+++ src/app/producto/[id]/ProductDetail.tsx
@@ -160,8 +160,8 @@ export default function ProductDetail({ product, images, reviews, avgRating, use
                 <img src="https://upload.wikimedia.org/wikipedia/commons/0/04/Visa.svg" alt="Visa" style={{ height: '16px', objectFit: 'contain' }} />
                 <img src="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg" alt="Mastercard" style={{ height: '20px', objectFit: 'contain' }} />
                 <img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/American_Express_logo_%282018%29.svg" alt="Amex" style={{ height: '20px', objectFit: 'contain' }} />
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk3MNgAJzgzJIAXwJaLuKMHnMZSBqSKpbFvA&s" alt="Nequi" style={{ height: '20px', objectFit: 'contain' }} />
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8y5JrQvWmZaGBHW3FzBOJnBXVLxYtSgRRow&s" alt="Daviplata" style={{ height: '20px', objectFit: 'contain' }} />
+                <img src="https://www.nequi.com.co/wp-content/themes/nequi/assets/img/logo.svg" alt="Nequi" style={{ height: '20px', objectFit: 'contain' }} />
+                <img src="https://www.daviplata.com/documents/20182/0/Logo_DaviPlata.png" alt="Daviplata" style={{ height: '20px', objectFit: 'contain' }} />
                 <span style={{ fontSize: '0.75rem', color: '#888' }}>PSE · Efecty · Baloto</span>
               </div>
             </div>
